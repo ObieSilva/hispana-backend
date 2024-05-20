91aa836945b16b8eecf31ca4824803dc04bd68c8
Obie Silva <obiesilva@gmail.com>
2024-05-20 09:55:23 -0400

    Integrate MongoDB

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config({ 
+    path: process.env.NODE_ENV === 'production' ? './.env.production' : './.env.test' 
+});  
+
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+const app = express();
+
+app.use(express.json()); // Middleware for parsing JSON
+
+app.get('/', (req, res) => { 
+    res.send('Hello, World!');
+});
+
+// Route to create a new user
+app.post('/users', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).send('User created successfully');
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Route to get all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// connect to MongoDB
+const connectionString = process.env.MONGODB_URI;
+
+mongoose.connect(connectionString)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
