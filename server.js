f2cb3bba6fe2543dcfa66292e0ec46e85386e9c6
Obie Silva <obiesilva@gmail.com>
2024-05-21 08:02:15 -0400

    Update server status and add cors

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ require('dotenv').config({
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); // Middleware for parsing JSON
 
 app.get('/', (req, res) => { 
@@ -15,19 +20,19 @@ app.get('/', (req, res) => {
 });
 
 // Route to create a new user
-app.post('/users', async (req, res) => {
+app.post('/api/v1/users', async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const newUser = new User({ name, email, password });
         await newUser.save();
-        res.status(201).send('User created successfully');
+        res.status(200).send('User created successfully');
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
 // Route to get all users
-app.get('/users', async (req, res) => {
+app.get('/api/v1/users', async (req, res) => {
     try {
         const users = await User.find();
         res.status(200).json(users);
